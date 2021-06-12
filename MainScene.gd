48de9c91c9dd78219163d4eca7cdf3a1b399fa6e
ObiDriftKenobi <gdpancake5@gmail.com>
2021-06-12 14:28:06 +0200

    Algorithm for detecting which sides can be glued together

--- MainScene.gd
+++ MainScene.gd
@@ -1,47 +1,53 @@
 extends Node2D
 
+class lineSort:
+	static func v2i(vcs):
+		var v1 = vcs[0]
+		var v2 = vcs[1]
+		var r = int(str(v1.x) + str(v1.y) + str(v2.x) + str(v2.y))
+		return r
+	
+	static func sort_ascending(a, b):
+		return v2i(a[0]) > v2i(b[0])
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func vecround(vec):
+	return Vector2(round(vec.x/5)*5,round(vec.y/5)*5)
+
+func sort_two_points(p1,p2):
+	if p1.x > p2.x:
+		return [vecround(p1),vecround(p2)]
+	elif p1.x < p2.x:
+		return [vecround(p2),vecround(p1)]
+	else:
+		if p1.y > p2.y:
+			return [vecround(p1),vecround(p2)]
+		else:
+			return [vecround(p2),vecround(p1)]
 
 func detect_position_overlaps():
 	var glueable = get_tree().get_nodes_in_group("glueable")
 	var objects_points = []
-	for p in glueable:
-		var poly = p.get_node("GlueablePiece").polygon
+	for p in range(len(glueable)):
+		var poly = glueable[p].get_node("GlueablePiece").polygon
 		for point in range(len(poly)):
-			poly[point] += p.get_node("GlueablePiece").global_position
+			poly[point] += glueable[p].get_node("GlueablePiece").global_position
 		objects_points.append(poly)
-		
-	var reference_copy = objects_points.duplicate()
-	
-	var current
-	var results = []
-	for i in range(len(objects_points)):
-		current = objects_points.pop_front()
-		for cur in current: # For all points of the selected object
-			for o in objects_points: # For all other objects
-				for e in o: # For all points of the forlooped object from above line
-					if cur.distance_to(e) < 0.5:
-						var first = i
-						var firstA = Array(reference_copy[first]).find(cur)
-						var second = reference_copy.find(o)
-						var secondA = Array(reference_copy[second]).find(e)
-						results.append([first,firstA,glueable[first].polygon[firstA]])
-						results.append([second,secondA,glueable[second].polygon[secondA]])
-		objects_points.append(current)
-	
-	# So now we have an array, results, containing points that touch, including duplicates.
-	# The first two array elements are references that can be traced back to get the rididbody,
-	# the third element is the Vector2 itself.
-	
-	# We need to test these Vector2's against each other and find out what series of Vector2's
-	# are sides that touch each other. IDK man.
+	var lines = []
+	for x in range(len(objects_points)):
+		for y in range(len(objects_points[x])):
+			lines.append([sort_two_points(objects_points[x][y], objects_points[x][(y+1)%len(objects_points[x])]),x,y])
+	lines.sort_custom(lineSort,"sort_ascending")
+	var dupe = [[]]
+	var glueables = []
+	for dupeLine in lines:
+		if dupe[0] == dupeLine[0]:
+			glueables.append([dupe,dupeLine])
+		dupe = dupeLine
+	for g in glueables:
+		get_children()[-1].points = PoolVector2Array([g[0][0][0], g[0][0][1]])
+		add_child(get_children()[-1].duplicate())
 	
 	
-	
-
 func _ready():
 	detect_position_overlaps()
 
