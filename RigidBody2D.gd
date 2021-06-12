7cd47ef26f4e47d483428b505237a3aca151d9d6
ObiDriftKenobi <gdpancake5@gmail.com>
2021-06-12 09:29:52 +0200

    + editor facility for making shapes, fixed collisions

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -8,15 +8,12 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_shape(PoolVector2Array([
-		Vector2(0,0),
-		Vector2(50,0),
-		Vector2(50,50),
-		Vector2(0,50),
-		]))
+	var shape = get_children()[1]
+	set_shape(shape.polygon)
 
 func set_shape(shape):
 	$GlueablePiece.polygon = shape
+	$GlueablePiece.position = Vector2.ZERO
 	var smallerShape = PoolVector2Array(Array(shape).duplicate())
 	var minx = 0
 	var maxx = 0
@@ -29,10 +26,17 @@ func set_shape(shape):
 		maxy = max(i.y,maxy)
 	var width = maxx-minx
 	var height = maxy-miny
+	print(Vector2(width,height))
 	var off = Vector2(width,height)*0.05 #0.05 is half of 1-0.9
 	for i in range(len(shape)):
 		smallerShape[i] *= 0.9
 		smallerShape[i] += off
-	$CollisionPolygon2D.polygon = smallerShape
+		smallerShape[i] -= Vector2(width,height)/2
+	$GlueablePiece.position -= Vector2(width,height)/2
+	position += Vector2(width,height)/2
+	var s = ConvexPolygonShape2D.new()
+	s.points = smallerShape
+	var so = create_shape_owner(self)
+	shape_owner_add_shape(so,s)
 	
 	
