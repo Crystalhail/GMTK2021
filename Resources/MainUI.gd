d41db652b558997ea79293650a7c7fdaf4d30b4a
ObiDriftKenobi <gdpancake5@gmail.com>
2021-06-12 09:42:12 +0200

    + add level select draft

--- Resources/MainUI.gd
+++ Resources/MainUI.gd
@@ -1,16 +1,16 @@
 extends TextureRect
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var level_button = preload("res://Resources/LevelButton.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for b in get_tree().get_nodes_in_group("hoverableButton"):
 		b.connect("mouse_entered",self,"hover_button",[b])
 		b.connect("mouse_exited",self,"unhover_button",[b])
+	for level in range(12):
+		var lev = level_button.instance()
+		lev.name = str(level+1) #IMPORTANT
+		$Levels/Levelgrid.add_child(lev)
 
 func hover_button(n):
 	move_child(n,get_child_count()-1)
